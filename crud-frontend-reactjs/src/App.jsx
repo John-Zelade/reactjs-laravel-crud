45adb3712e4d7d90ff15dbe8008ae8ead23ba1bc
John-Zelade <https://johnzeladehigo@gmail.com>
2023-05-16 12:08:46 +0800

    add crud-frontend-reactjs

---
+++ crud-frontend-reactjs/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import {StudRecTable} from './stud_components/StudentRecordTb';
+
+
+const App = ()=>{
+
+    return(
+        <div className='App'>
+           <StudRecTable/>
+        </div>
+    );
+}
+export default App;
